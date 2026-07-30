52e52626b4fc2609338db430f7bec08ef43593db
Tammam20 <whymewhyalwaysme@protonmail.com>
2026-07-30 11:48:03 +0300

    hello

---
+++ finix/configuration.nix
@@ -0,0 +1,218 @@
+{ config, pkgs, ... }:
+
+{
+
+
+imports = with modules; [
+    fstrim
+    gnome-keyring
+    gvfs
+    micro
+    niri
+    noisetorch
+    pmount
+    seahorse
+    sway
+    thermald
+	fwupd
+	seatd
+	upower
+	brightnessctl
+	seahorse
+	zzz
+	limine
+	power-profiles-daemon
+	
+  ];
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  finit.runlevel = 3;
+
+  finit.services.nix-daemon = {
+    environment.CURL_CA_BUNDLE = config.security.pki.caBundle;
+  };
+
+  services.nix-daemon = {
+    enable = true;
+    settings = {
+      experimental-features = [ "nix-command" "flakes" ];
+      trusted-users = [
+        "root"
+        "@wheel"
+      ];
+    };
+  };
+
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  programs = {
+    limine = {
+      enable = true;
+      settings.editor_enabled = true; # Disable on systems that need security
+    };
+
+    sudo.enable = true;
+	micro.enable = true;
+	micro.defaultEditor = true;
+	nano.enable = true;
+	nano.defaultEditor = false;
+    bash.enable = true;
+    sway.enable = true;
+    pmount.enable = true;
+    seahorse.enable = true;
+    gnome-keyring.enable = true;
+    brightnessctl.enable = true;
+    zzz.enable = true;
+  };
+
+  services = {
+    polkit.enable = true;
+
+    sysklogd.enable = true;
+
+    dbus.enable = true;
+
+   	mdevd.enable = true;
+    #gardendevd.enable = true;
+
+    dhcpcd.enable = true;
+    rtkit.enable = true;
+    thermald.enable = true;
+    gvfs.enable = true;
+    power-profiles-daemon.enable = true;
+    fstrim.enable = true;
+    fstrim.interval = "daily";
+    upower.enable = true;
+    fwupd.enable = true;
+
+    iwd.enable = true;
+    seatd.enable = true;
+    /*greetd = {
+    enable = true;
+    settings = {
+   default_session = {
+   command = "${pkgs.tuigreet}/bin/tuigreet";
+}; 
+};
+};*/
+ };
+
+  xdg.portal.enable = true;
+  fonts = {
+  fontconfig.enable = true;
+  enableDefaultPackages = true;
+  packages = with pkgs; [
+  nerd-fonts.fira-code
+];
+};
+
+  networking.hostName = "t480"; # Define your hostname.
+
+  # Set your time zone.
+  time.timeZone = "Asia/Baghdad";
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.tammam = {
+    isNormalUser = true;
+    description = "Tammam Faris";
+    extraGroups = [ 
+     "wheel" 
+     "video" 
+      config.services.seatd.group 
+      config.hardware.i2c.group
+      #config.hardware.uinput.group
+      "audio"
+      # "gamemode"
+     # "incus-admin"
+      "input"
+     # "keyd"
+     # "kvm"
+     # "vboxusers"
+      "video"
+      "wheel"
+        ];
+    password = "$6$1aOsu4xRRBDJWA3O$yUIEmHIzcJ2KczaW1RcVc6ji.vtCXND57iIqt8NfZHL7326zAViJrTGZriK.e1/5JovKqh/wElp7VmQB2TbLA."; #pass=vitrial
+    packages = with pkgs; [];
+  };
+  
+hardware.graphics.enable = true;
+hardware.graphics.enable32Bit = true;
+ # https://wiki.nixos.org/wiki/Accelerated_Video_Playback#Intel
+  hardware.graphics.extraPackages = [ pkgs.intel-media-driver ];
+  hardware.graphics.extraPackages32 = [ pkgs.pkgsi686Linux.intel-media-driver ];
+  
+hardware.console.keyMap = "de";
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    nano
+    git
+    nixos-rebuild-ng
+    iputils
+    iproute2
+    brave
+    chezmoi
+    gh
+    keepassxc
+    foot
+    cliphist
+    wmenu
+    wl-clipboard
+    wf-recorder
+    grim
+    slurp
+    swaybg
+    sway-audio-idle-inhibit
+    swayidle
+    swaynotificationcenter
+    brightnessctl
+    playerctl
+    papirus-icon-theme
+    gnome-themes-extra
+    lxqt.lxqt-policykit
+    wl-clip-persist
+    waybar
+    wev
+  ];
+
+  # TODO: shouldn't this just be included by default?
+    services.mdevd.hotplugRules = lib.mkMerge [
+      (lib.mkAfter ''
+        SUBSYSTEM=input;.* root:input 660
+        SUBSYSTEM=sound;.* root:audio 660
+      '')
+  
+      ''
+        grsec       root:root 660
+        kmem        root:root 640
+        mem         root:root 640
+        port        root:root 640
+        console     root:tty 600 @chmod 600 $MDEV
+        card[0-9]   root:video 660 =dri/
+  
+        # alsa sound devices and audio stuff
+        pcm.*       root:audio 0660 =snd/
+        control.*   root:audio 0660 =snd/
+        midi.*      root:audio 0660 =snd/
+        seq         root:audio 0660 =snd/
+        timer       root:audio 0660 =snd/
+  
+        adsp        root:audio 0660 >sound/
+        audio       root:audio 0660 >sound/
+        dsp         root:audio 0660 >sound/
+        mixer       root:audio 0660 >sound/
+        sequencer.* root:audio 0660 >sound/
+  
+        event[0-9]+ root:input 660 =input/
+        mice        root:input 660 =input/
+        mouse[0-9]+ root:input 660 =input/
+  
+        rfkill      root:${config.services.seatd.group} 660
+      ''
+    ];
+  
+}
---
+++ flake.nix
@@ -0,0 +1,77 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    finix.url = "github:finix-community/finix?ref=main";
+    community-modules.url = "github:finix-community/community-modules"; # "github:finix-community/community-modules";
+   # sops-nix.url = "github:Mic92/sops-nix";
+   # sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+   # noctalia.url = "github:noctalia-dev/noctalia";
+   # noctalia.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      finix,
+      community-modules,
+ #     sops-nix,
+ #     noctalia,
+    }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+
+        config.allowUnfree = true;
+       /* overlays = [
+          # self.overlays.xlibre
+          self.overlays.custom
+          sops-nix.overlays.default
+        ];*/
+      };
+    in
+    {
+      /*overlays.xlibre = import ./xlibre.nix;
+      overlays.custom = final: prev: {
+        swayidle = prev.swayidle.override { systemdSupport = false; };
+        xdg-desktop-portal = prev.xdg-desktop-portal.override { enableSystemd = false; };
+        xwayland-satellite = prev.xwayland-satellite.override { withSystemd = false; };
+
+        # noctalia v5!
+        noctalia-shell = noctalia.packages.x86_64-linux.default;
+      };*/
+
+      # finix
+      nixosConfigurations.t480 = finix.lib.finixSystem {
+        inherit (pkgs) lib;
+
+        specialArgs.modules = {
+          inherit (community-modules.nixosModules)
+            cups
+            dinit
+            laptop
+            # turnstile
+            ;
+
+          #noisetorch = toString nixpkgs + "/nixos/modules/programs/noisetorch.nix";
+          #flirc = toString nixpkgs + "/nixos/modules/hardware/flirc.nix";
+          #xfconf = ./xfconf.nix;
+        };
+
+        modules = [
+
+          ./finix/configuration.nix
+          ./finix/hardware-configuration.nix
+          #./pam.nix
+          #./sops
+          # ./openbox.nix
+          # ./plasma.nix
+          # ./xfce.nix
+          # ./podman.nix
+
+          { nixpkgs.pkgs = pkgs; }
+        ];
+      };
+    };
+}
