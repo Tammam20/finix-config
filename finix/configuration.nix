a65cb433abd586ac7966f4241449d7c43a1a5065
Tammam20 <whymewhyalwaysme@protonmail.com>
2026-08-02 10:30:07 +0300

    update

--- finix/configuration.nix
+++ finix/configuration.nix
@@ -223,19 +223,6 @@ users.groups.audio.gid = config.ids.gids.audio;
   	];
     };
   
-
-  # audio fix
-  environment.etc."profile.d/pipewire-session.sh" = {
-        text = ''
-          if [ -n "$XDG_RUNTIME_DIR" ] && [ ! -e "$XDG_RUNTIME_DIR/pipewire-session.lock" ]; then
-            if ( set -o noclobber; : > "$XDG_RUNTIME_DIR/pipewire-session.lock" ) 2>/dev/null; then
-              ${config.programs.pipewire.package}/bin/pipewire >/dev/null 2>&1 &
-              ${config.programs.pipewire.package}/bin/pipewire-pulse >/dev/null 2>&1 &
-              ${lib.optionalString config.programs.wireplumber.enable "${config.programs.wireplumber.package}/bin/wireplumber >/dev/null 2>&1 &"}
-            fi
-          fi
-        '';
-      };
   
   # List packages installed in system profile. To search, run:
   # $ nix search wget
