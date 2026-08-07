5a4321f3eae733ce662d6b456c2c74db1b36b5ad
Tammam20 <whymewhyalwaysme@protonmail.com>
2026-08-07 22:37:05 +0300

    update

--- finix/configuration.nix
+++ finix/configuration.nix
@@ -5,6 +5,7 @@
 
 imports = with modules; [
     fstrim
+    fastfetch
     #bluetooth
     gnome-keyring
     gvfs
@@ -69,12 +70,32 @@ imports = with modules; [
   boot.loader.efi.canTouchEfiVariables = true;
 
   programs = {
-    limine = {
-      enable = true;
-      settings.editor_enabled = true; # Disable on systems that need security
-    };
+   limine = {
+    enable = true;
+	settings = { 
+	editor_enabled = true; # Disable on systems that need security
+    interface_resolution = "1920x1080";
+    wallpaper = [ ./limine-wallpaper.png ];
+    wallpaper_style = "stretched";
+    backdrop = "16181d";
+    term_background = "c0161818";
+    term_foreground = "dcdee4";
+    term_background_bright = "d31f30";
+    term_foreground_bright = "ffffff";
+    term_palette = "16181d;d31f30;7d7d7d;de3040;3a3f4b;e84050;9aa0ac;dcdee4";
+    term_palette_bright = "2a2d35;e84050;9aa0ac;e84050;5a6070;f06070;c0c4cc;ffffff";
+    term_margin = 24;
+    term_margin_gradient = 24;
+    interface_branding = "finix";
+    interface_branding_colour = "e84050";
+    interface_help_colour = "7d7d7d";
+    interface_help_colour_bright = "dcdee4";
+    interface_help_hidden = false;
+   	 };
+   };
 
     sudo.enable = true;
+    fastfetch.enable = true;
 	micro.enable = true;
 	micro.defaultEditor = true;
 	nano.enable = true;
@@ -233,12 +254,12 @@ users.groups.audio.gid = config.ids.gids.audio;
 
 
     # libvirt
-    finit.services.libvirtd = {
+   /* finit.services.libvirtd = {
           description = "libvirt virtualisation daemon";
           runlevels   = "2345";
           conditions  = [ "service/syslogd/ready" ];
           command     = "${pkgs.libvirt}/bin/libvirtd";
-        };
+        };*/
   
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -282,14 +303,14 @@ users.groups.audio.gid = config.ids.gids.audio;
     xfconf
     btop
     #atop
-    fastfetch
+	#fastfetch
     lm_sensors
     dconf-editor
-    virt-manager
+    /*virt-manager
 	qemu
     virt-viewer
     spice-gtk
-	virtiofsd
+	virtiofsd*/
 	file-roller
   	shellcheck
   	powertop
