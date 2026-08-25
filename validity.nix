da8ca08d5ecca14bb3f9227ce414d2c2866b5adf
Tammam20 <whymewhyalwaysme@protonmail.com>
2026-08-25 22:30:24 +0300

    update

--- validity.nix
+++ validity.nix
@@ -50,21 +50,21 @@ imports = [
 		}
 	];
 	
-	 services.polkit.extraConfig = ''
-      polkit.addRule(function(action, subject) {
-        if (action.id.startsWith("net.reactivated.fprint.device.")) {
-          var groups = ${builtins.toJSON cfg.extraGroups};
-
-          if (groups.some(function(group) {
-            return subject.isInGroup(group);
-          })) {
-            return polkit.Result.YES;
-          }
-        }
-
-        return polkit.Result.NOT_HANDLED;
-      });
-    '';
+	 services.polkit.extraConfig = lib.optionalString (config.services.seatd.group != [ ]) ''
+	       polkit.addRule(function(action, subject) {
+	         if (action.id.startsWith("net.reactivated.fprint.device.")) {
+	           var groups = ${builtins.toJSON config.services.seatd.group};
+	 
+	           if (groups.some(function(group) {
+	             return subject.isInGroup(group);
+	           })) {
+	             return polkit.Result.YES;
+	           }
+	         }
+	 
+	         return polkit.Result.NOT_HANDLED;
+	       });
+	     '';
     
 	providers.resumeAndSuspend.hooks = { 
 	python-validity.enable = true;
